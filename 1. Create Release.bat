2f362276b5891e94841a3d338dfd65df458e6fb9
hol353 <hol353@31490d77-854b-a441-874d-f8801a3780c9>
2011-02-09 05:54:37 +0000

    First revision of a whole bunch of projects
    git-svn-id: file://fsqld3-tw/SE-Home/hol353/SVNRepository/ApsoilRelease@138 31490d77-854b-a441-874d-f8801a3780c9

---
+++ 1. Create Release.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Now combine apsimsetup.msi and setup.exe into a self extracting installation.
+rem This uses IExpress described here:
+rem http://www.itscodingtime.com/post/Combine-Setup-MSI-and-EXE-into-a-single-package-with-IExpress.aspx
+
+echo REMEMBER TO RECOMPILE THE .MSI!!!!
+..\IExpress32\iexpress /N Apsoil.sed
+pause
