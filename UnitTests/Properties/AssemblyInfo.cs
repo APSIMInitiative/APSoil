480ee47a8e9e06da19dff1986ebbb61e3ecd8982
Dean Holzworth <Dean.Holzworth@csiro.au>
2021-04-08 13:42:22 +1000

    Added unit test and added missing files to service.

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CSIRO")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © CSIRO 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4827ceb3-7dbb-4316-ac34-5cd6ce670dc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Apsoil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var apsoil = new Service();
+            var s = apsoil.SoilChartPNG("Soils/Australia/Queensland/Darling Downs and Granite Belt/Grey Vertosol (Goondiwindi No862)");
+
+            using (var fs = new FileStream(@"C:\Users\hol353\Temp\WebSites\test.png", FileMode.Create, FileAccess.Write))
+            {
+                fs.Write(s, 0, s.Length);
+            }
+        }
+    }
+}
