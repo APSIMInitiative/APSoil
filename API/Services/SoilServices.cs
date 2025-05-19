4e5854d071df7cf6c31d3b654b3d94f3f5c50421
Dean Holzworth <Dean.Holzworth@csiro.au>
2025-05-19 14:33:36 +1000

    Fixed PAW calculation

--- API/Services/SoilServices.cs
+++ API/Services/SoilServices.cs
@@ -181,9 +181,13 @@ public static class Soil
             xf = null;
         }
 
-        IReadOnlyList<double> swMapped = sw.SWMappedTo(thickness, soil.Water.Thickness, ll);
         if (swIsGrav)
-            swMapped = swMapped.ConvertGravimetricToVolumetric(soil.Water.BD);
+        {
+            IReadOnlyList<double> bdMapped = soil.Water.BD.MappedTo(soil.Water.Thickness, thickness);
+            sw = sw.ConvertGravimetricToVolumetric(bdMapped);
+        }
+
+        IReadOnlyList<double> swMapped = sw.SWMappedTo(thickness, soil.Water.Thickness, ll);
         var pawByLayer = SoilUtilities.CalcPAWC(soil.Water.Thickness, ll, swMapped, xf);
         return MathUtilities.Multiply(pawByLayer, soil.Water.Thickness).Sum();
     }
@@ -202,7 +206,7 @@ public static class Soil
         sw.Add(0.0);                     // 3rd pseudo layer below profile.
         thickness.Add(thickness.Last()); // 1st pseudo layer below profile.
         thickness.Add(thickness.Last()); // 2nd pseudo layer below profile.
-        thickness.Add(3000);             // 3rd pseudo layer below profile.
+        thickness.Add(300);             // 3rd pseudo layer below profile.
 
         var llMapped = ll.MappedTo(toThickness, thickness);
         var swMM = sw.LowerConstraint(llMapped, startIndex: values.Count)
